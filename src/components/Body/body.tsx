1e9d048d04aed3fd9b2fa212e8a5e3e462da0cee
Victor Hugo B <victorhugobeppler@gmail.com>
2023-02-02 08:49:49 -0300

    body

---
+++ src/components/Body/body.tsx
@@ -0,0 +1,10 @@
+export function Body() {
+    return (
+        <div className="w-full h-full">
+            <div className="w-3/6 h-56 ">
+                <h1 className="p-32 font-bold text-2xl">Victor Hugo Beppler Pereira</h1>
+                {/* <div className="border-b border-white solid"/> */}
+            </div>
+        </div>
+    )
+}
