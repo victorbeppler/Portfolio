53cd4f7b37f2baa9310c1d88ef87466c323ae93c
Victor Hugo B <victorhugobeppler@gmail.com>
2023-02-02 22:59:06 -0300

    Criando Portifólio

--- src/components/Body/body.tsx
+++ src/components/Body/body.tsx
@@ -3,7 +3,7 @@ export function Body() {
         <div className="w-full h-full">
             <div className="w-3/6 h-56 ">
                 <h1 className="p-32 font-bold text-2xl">Victor Hugo Beppler Pereira</h1>
-                {/* <div className="border-b border-white solid"/> */}
+                <div className="w-1 h-0 border"/>
             </div>
         </div>
     )
