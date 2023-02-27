5e7801f2a82f8b9647bb8ab2dce6ee3407ea9763
Victor Hugo B <victorhugobeppler@gmail.com>
2023-02-27 20:54:59 -0300

    Build(Create Porjects and Cards Components)

---
+++ src/components/Projects/styles.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Wrap = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+export const Title = styled.h2`
+  font-size: 1.5rem;
+  width: auto;
+  color: white;
+  border-bottom: 2px solid white;
+  padding-bottom: 1rem;
+  margin-bottom: 2rem;
+  text-align: center;
+`;
+
+export const Container = styled.div`
+  padding-top: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
